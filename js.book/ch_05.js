4fbc40cde16cc07f6306cb08decd2f89593f7f36
keepgoingautumn <lms301301@naver.com>
2026-03-11 21:55:48 +0900

    함수파트
    함수파트 공부

---
+++ js.book/ch_05.js
@@ -0,0 +1,24 @@
+
+
+
+
+// function addNumber(num1, num2){
+//     let sum = num1 + num2;
+//     return sum;
+// }
+
+// let result = addNumber(2,3);
+// console.log(result);
+
+
+        function sumMulti(x,y){
+            if(x === y){
+                return x * y;
+            }else{
+                return x + y;
+            }
+
+            
+        }
+        let result = sumMulti(5,10);
+        console.log(result);
